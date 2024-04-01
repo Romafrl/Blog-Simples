464efd0f5b9b3f5bea47a231c770aa0bd8778012
Pedro André <143569804+Romafrl@users.noreply.github.com>
2024-03-31 22:19:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-function toggleDarkMode() {
-    const body = document.body;
-    body.classList.toggle("dark-mode");
 
-    const isDarkMode = body.classList.contains("dark-mode");
-    localStorage.setItem("darkMode", isDarkMode);
-}
-
-const savedDarkMode = localStorage.getItem("darkMode");
-if (savedDarkMode === "true") {
-    document.body.classList.add("dark-mode");
-}
